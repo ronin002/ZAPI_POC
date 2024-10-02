701554807c9246f33d26bb2f75f71780229b4c85
Edson Márcio <edson.marcio7@gmail.com>
2024-10-02 10:28:52 -0300

    TestVersion TesteListJson

--- Controllers/ZurichController.cs
+++ Controllers/ZurichController.cs
@@ -23,5 +23,38 @@ namespace ZurichAPI.Controllers
         {
             return null;
         }
+
+        [AllowAnonymous]
+        [HttpGet("api/v1/zurich/")]
+        public IActionResult GetListTest()
+        {
+            List<ZUser> users = new List<ZUser>();
+
+            ZUser user = new ZUser();
+            user.Name = "Edson";
+            user.Idade = 30;
+
+            ZUser user1= new ZUser();
+            user1.Name = "Marcos";
+            user1.Idade = 27;
+
+
+            ZUser user2 = new ZUser();
+            user2.Name = "Felipe";
+            user2.Idade = 22;
+
+
+            users.Add(user);
+            users.Add(user1);
+            users.Add(user2);
+
+            return Ok(users);
+        }
+    }
+
+    public class ZUser
+    {
+        public string Name { get; set; }
+        public int Idade { get; set; }
     }
 }
